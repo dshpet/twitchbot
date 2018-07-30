2ef8e2370f4e99c4fa49d6cff8045837d8188d82
Dmitry Shpetnyi <shpetnyi@g5software.com>
2018-07-30 20:57:44 +0300

    slightly fix the bot. Add emote hashing

--- twitch_test/commands/TwitchEmote.py
+++ twitch_test/commands/TwitchEmote.py
@@ -3,19 +3,22 @@
 
 from .BaseCommand import BaseCommand
 from urllib import request
-from bs4 import BeautifulSoup
-from random import choice
+import random
 import json
+import hashlib
 
 class TwitchEmote(BaseCommand):
     """Gets a random twitch emote from twitchemotes api"""
 
     def respond(self, message, sender):
-      url = "https://twitchemotes.com/api_cache/v2/global.json"
+      url = "https://twitchemotes.com/api_cache/v3/global.json"
       response = request.urlopen(url).read()
       parsed = json.loads(response.decode('utf-8'))
-      global_emotes = list(parsed['emotes'].keys())
-      random_emote = choice(global_emotes)
+      global_emotes = list(parsed.keys())
+
+      hash = hashlib.md5(sender.encode()).hexdigest()
+      random.seed(hash)
+      random_emote = random.choice(global_emotes)
       
       return "You are a " + random_emote + ", @" + sender
 
